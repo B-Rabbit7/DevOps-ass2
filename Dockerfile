92d8e55105fc7521a6ed8d0f73f0f35684193056
B-Rabbit7 <snouri3@my.bcit.ca>
2023-10-07 14:58:14 -0700

    updated files

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ RUN apt-get update && apt-get install -y git
 
 # Clone your GitHub repository
 RUN git clone https://github.com/B-Rabbit7/DevOps-ass2.git /usr/src/app
-
+WORKDIR /usr/src/app/DevOps-ass2
 COPY package*.json ./
 RUN npm install
 # Expose the port your app will run on (adjust this if needed)
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 'use strict';
- 
+
 const express = require('express');
- 
+const path = require('path'); // Import the 'path' module
+
 // Constants
-const PORT = 8000;
+const PORT = 80;  // Change to 80 to match the exposed port
 const HOST = '0.0.0.0';
- 
+
 // App
 const app = express();
+
+// Define the directory where your static files (including index.html) are located
+const staticDir = path.join(__dirname, 'public'); // Change 'public' to your actual directory name
+
+// Serve static files from the specified directory
+app.use(express.static(staticDir));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  // Send the index.html file as the response
+  res.sendFile(path.join(staticDir, 'index.html'));
 });
- 
+
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
